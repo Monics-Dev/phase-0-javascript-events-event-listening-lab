bf5a7a9091bd13b831ac6d2ce83e6cc9e177f4ce
Monica <monijerobon@gmail.com>
2023-03-29 08:39:36 +0300

    Done

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const input =document.getElementById("button");
-function clickAlert(){
-alert("I was clicked");
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+function addingEventListener(addEventlistener){
+    addEventlistener("click", addEventlistener);
+    if (addEventlistener === click){
+
+    }else(addingEventListener = addEventlistener)
+    return addEventlistener
+
 }
-function addingEventListener(){
-input.addEventListener("click",clickAlert)
-}
+addingEventListener()
